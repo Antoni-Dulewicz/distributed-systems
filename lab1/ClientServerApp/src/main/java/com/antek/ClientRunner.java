038fb14d1cda04053cd1307184928b89cbd1c4d2
Antoni Dulewicz <antoni.dulewicz3@gmail.com>
2025-03-15 16:58:20 +0100

    lab1 - multicast added

--- lab1/ClientServerApp/src/main/java/com/antek/ClientRunner.java
+++ lab1/ClientServerApp/src/main/java/com/antek/ClientRunner.java
@@ -4,10 +4,7 @@ import com.antek.model.Data;
 import com.antek.model.Requests;
 
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.*;
 import java.util.Scanner;
 
 public class ClientRunner{
@@ -18,11 +15,22 @@ public class ClientRunner{
     private Socket socket;
     private DatagramSocket udpSend;
     private DatagramSocket udpListen;
+    private MulticastSocket multicastSocket;
+    private InetAddress multicastAddress;
+
+    private static final int MULTICAST_PORT = 5000;
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 8080;
+
+
+    private void initializeMulticast() throws IOException {
+        multicastSocket = new MulticastSocket(MULTICAST_PORT);
+        multicastAddress = InetAddress.getByName("230.0.0.0");
+        multicastSocket.joinGroup(multicastAddress);
+        System.out.println("[MULTICAST]: client connected to multicast");
+    }
 
     public void run() {
-        //server address and port
-        String SERVERIP = "127.0.0.1";
-        int SERVERPORT = 8080;
 
         //starting client
         System.out.println("[CLIENT]: client started");
@@ -31,9 +39,9 @@ public class ClientRunner{
         try{
 
             //create TCP socket
-            socket = new Socket(SERVERIP, SERVERPORT);
+            socket = new Socket(SERVER_IP, SERVER_PORT);
 
-            System.out.println("[CLIENT TCP]: connected to server at " + SERVERIP + ":" + SERVERPORT);
+            System.out.println("[CLIENT TCP]: connected to server at " + SERVER_IP + ":" + SERVER_PORT);
             System.out.println("[CLIENT TCP]: local socket info: " + socket.getLocalAddress() + ":" + socket.getLocalPort());
 
             //create UDP sockets for sending and listening - sending is on the same port as TCP
@@ -58,6 +66,10 @@ public class ClientRunner{
             Data response = (Data) in.readObject();
             System.out.println("[SERVER]: client <"+response.message()+"> connected, type 'enter' to send, type 'exit' to disconnect, type 'u' to use UDP");
 
+
+            initializeMulticast();
+            //listening for multicast messages
+            new Thread(this::listenForMulticastMessages).start();
             //listening for messages sent by server by TCP
             new Thread(this::listenForMessagesTcp).start();
             //listening for messages sent by server by UDP
@@ -90,14 +102,39 @@ public class ClientRunner{
                         //if multiline input is empty we don't send data
                         if(!multiLineInput.isEmpty()){
                             byte[] sendBuffer = multiLineInput.toString().getBytes();
-                            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName(SERVERIP), SERVERPORT);
+                            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName(SERVER_IP), SERVER_PORT);
                             udpSend.send(sendPacket);
                             break;
                         }
 
                     }
 
-                } else if ("exit".equalsIgnoreCase(userInput)) {
+                }else if ("m".equalsIgnoreCase(userInput)) {
+                    while(true){
+                        System.out.print("[you (MULTICAST)]: ");
+                        //using multiline to be able to send multiline data - ASCII art
+                        StringBuilder multiLineInput = new StringBuilder();
+                        String line;
+
+                        while(true){
+                            line = scanner.nextLine();
+                            if("end".equalsIgnoreCase(line)){
+                                break;
+                            }
+
+                            multiLineInput.append(line).append("\n");
+                        }
+                        //if multiline input is empty we don't send data
+                        if(!multiLineInput.isEmpty()){
+                            byte[] sendBuffer = multiLineInput.toString().getBytes();
+                            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, multicastAddress, MULTICAST_PORT);
+                            multicastSocket.send(sendPacket);
+                            break;
+                        }
+
+                    }
+
+                }else if ("exit".equalsIgnoreCase(userInput)) {
                     running = false;
 
                     System.out.println("[CLIENT]: disconnecting...");
@@ -124,7 +161,6 @@ public class ClientRunner{
         }
 
     }
-
 
     private void listenForMessagesTcp(){
         try{
@@ -162,6 +198,29 @@ public class ClientRunner{
         }
     }
 
+    private void listenForMulticastMessages(){
+        try{
+
+            while(running){
+                byte[] receiveBuffer = new byte[20000];
+
+                DatagramPacket receivePacket =
+                        new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                multicastSocket.receive(receivePacket);
+
+                System.out.println();
+
+                String msg = new String(receivePacket.getData(),0,receivePacket.getLength());
+                System.out.println("[MULTICAST]: " + msg);
+                System.out.print("[you]: " );
+            }
+        } catch (Exception e) {
+            System.out.print("");
+        }
+    }
+
+
+
     private void closeConnections() {
         try {
             if (socket != null && !socket.isClosed()) {
@@ -172,6 +231,10 @@ public class ClientRunner{
             }
             if (udpSend != null && !udpSend.isClosed()) {
                 udpSend.close();
+            }
+            if (multicastSocket != null && !multicastSocket.isClosed()) {
+                multicastSocket.leaveGroup(multicastAddress);
+                multicastSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
